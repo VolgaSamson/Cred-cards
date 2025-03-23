44c4e6f96e70f25d2cfc7a1e5d25a8e7a4e39b40
Volga Samson <volgasamson@gmail.com>
2025-03-23 14:21:23 +0530

    updated-frontend-changes

---
+++ src/app/components/footer/footer.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+import styles from './footer.module.css'; 
+
+const Footer: React.FC = () => {
+  return (
+    <footer className={styles.footer}>
+        
+    </footer>
+  );
+};
+
+export default Footer;
